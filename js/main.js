d33958b21dfe546986313f4729083b973425c815
codu2 <82283517+codu2@users.noreply.github.com>
2021-08-18 09:46:56 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$('.sec3-slider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 2,
+    speed: 1500
+});
+
+const r = rolly({
+    view: document.querySelector('.app'),
+    native: true,
+    // other options
+});
+r.init();
+
+AOS.init({
+    duration: 1500
+});
